effc039cadfc06569f39069345ce010a2f492395
blargwargyarg <chujoe26@gmail.com>
2020-08-25 13:09:25 -0400

    Uploaded Version 1.0.3

---
+++ HealthOnKill/HealthOnKillPatch.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
+using TaleWorlds.Engine;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace HealthOnKill
+{
+    [HarmonyPatch(typeof(Mission))]
+    internal static class HealthOnKillPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("HandleMissileCollisionReaction")]
+        public static bool Prefix1(int missileIndex, ref Mission.MissileCollisionReaction collisionReaction,
+            MatrixFrame attachLocalFrame, Agent attackerAgent, Agent attachedAgent, bool attachedToShield,
+            sbyte attachedBoneIndex, MissionObject attachedMissionObject, Vec3 bounceBackVelocity, Vec3 bounceBackAngularVelocity, int forcedSpawnIndex)
+        {
+            if (attachedAgent != null)
+            {
+                if (attachedAgent.IsMount && HealthOnKillSettings.Instance.MountProjectiles)
+                {
+                    if ((attachedAgent.RiderAgent.IsMainAgent && HealthOnKillSettings.Instance.MainProjectiles ||
+                        (attachedAgent.RiderAgent.IsHero && HealthOnKillSettings.Instance.HeroProjectiles) ||
+                        HealthOnKillSettings.Instance.AIProjectiles) &&
+                        (!HealthOnKillSettings.Instance.FriendlyProjectiles || (Agent.Main != null && attachedAgent.RiderAgent.IsFriendOf(Agent.Main)))
+                        ) collisionReaction = Mission.MissileCollisionReaction.BecomeInvisible;
+                }
+                else if (attachedAgent.IsMainAgent && (HealthOnKillSettings.Instance.MainProjectiles || HealthOnKillSettings.Instance.MainStick))
+                    collisionReaction = Mission.MissileCollisionReaction.BecomeInvisible;
+                else if (attachedAgent.IsHero && HealthOnKillSettings.Instance.HeroProjectiles)
+                {
+                    if (!HealthOnKillSettings.Instance.FriendlyOnly || (Agent.Main != null && attachedAgent.IsFriendOf(Agent.Main)))
+                        collisionReaction = Mission.MissileCollisionReaction.BecomeInvisible;
+                }
+                else if (HealthOnKillSettings.Instance.AIProjectiles && !HealthOnKillSettings.Instance.FriendlyOnly || (Agent.Main != null && attachedAgent.IsFriendOf(Agent.Main)))
+                    collisionReaction = Mission.MissileCollisionReaction.BecomeInvisible;
+            }
+            return true;
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch("RegisterBlow")]
+        private static bool Prefix2(Agent attacker, Agent victim, GameEntity realHitEntity, ref Blow b, ref AttackCollisionData collisionData)
+        {
+            if (attacker == null || victim == null) return;
+            if (attacker.IsMainAgent)
+                b.InflictedDamage *= HealthOnKillSettings.Instance.DPlayerMultiplier;
+            else if (attacker.IsHero)
+            {
+                if (!HealthOnKillSettings.Instance.FriendlyOnly || (Agent.Main != null && attacker.IsFriendOf(Agent.Main)))
+                    b.InflictedDamage *= HealthOnKillSettings.Instance.DHeroMultiplier;
+            }
+            else if (!HealthOnKillSettings.Instance.FriendlyOnly || (Agent.Main != null && attacker.IsFriendOf(Agent.Main)))
+                b.InflictedDamage *= HealthOnKillSettings.Instance.DAIMultiplier;
+            if (b.IsMissile())
+            {
+                if (victim.IsMount && HealthOnKillSettings.Instance.MountProjectiles)
+                {
+                    if ((victim.RiderAgent.IsMainAgent && HealthOnKillSettings.Instance.MainProjectiles ||
+                        (victim.RiderAgent.IsHero && HealthOnKillSettings.Instance.HeroProjectiles) ||
+                        HealthOnKillSettings.Instance.AIProjectiles) &&
+                        (!HealthOnKillSettings.Instance.FriendlyProjectiles || (Agent.Main != null && victim.RiderAgent.IsFriendOf(Agent.Main)))
+                        ) return false;
+                }
+                else if (victim.IsMainAgent && HealthOnKillSettings.Instance.MainProjectiles)
+                    return false;
+                else if (victim.IsHero && HealthOnKillSettings.Instance.HeroProjectiles)
+                {
+                    if (!HealthOnKillSettings.Instance.FriendlyProjectiles || (Agent.Main != null && victim.IsFriendOf(Agent.Main)))
+                        return false;
+                }
+                else if (HealthOnKillSettings.Instance.AIProjectiles && !HealthOnKillSettings.Instance.FriendlyProjectiles || (Agent.Main != null && victim.IsFriendOf(Agent.Main)))
+                    return false;
+            }
+            return true;
+        }
+        [HarmonyPostfix]
+        [HarmonyPatch("RegisterBlow")]
+        private static void Postfix(Agent attacker, Agent victim, GameEntity realHitEntity, Blow b, ref AttackCollisionData collisionData)
+        {
+            if (attacker == null || victim == null)
+            {
+                return;
+            }
+            if (!HealthOnKillPatch.BlowWillDoDamageToVictim(attacker, victim, b, ref collisionData) || victim.Health > 0f)
+            {
+                return;
+            }
+            float toAdd = 0f;
+            if (attacker.IsMainAgent)
+                toAdd = HealthOnKillSettings.Instance.HealthOnKillAmount * HealthOnKillSettings.Instance.PlayerMultiplier;
+            else if (attacker.IsHero)
+            {
+                if (!HealthOnKillSettings.Instance.FriendlyOnly || (Agent.Main != null && attacker.IsFriendOf(Agent.Main)))
+                    toAdd = HealthOnKillSettings.Instance.HealthOnKillAmount * HealthOnKillSettings.Instance.HeroMultiplier;
+            }
+            else if (!HealthOnKillSettings.Instance.FriendlyOnly || (Agent.Main != null && attacker.IsFriendOf(Agent.Main)))
+                toAdd = HealthOnKillSettings.Instance.HealthOnKillAmount * HealthOnKillSettings.Instance.AIMultiplier;
+            if (toAdd != 0f)
+            {
+                attacker.Health = ((attacker.Health + toAdd < attacker.HealthLimit) ? (attacker.Health + toAdd) : attacker.HealthLimit);
+                if (attacker.HasMount)
+                {
+                    toAdd = HealthOnKillSettings.Instance.HealthOnKillAmount * HealthOnKillSettings.Instance.MountMultiplier;
+                    attacker.MountAgent.Health = ((attacker.MountAgent.Health + toAdd < attacker.MountAgent.HealthLimit) ? (attacker.MountAgent.Health + toAdd) : attacker.MountAgent.HealthLimit);
+                }
+            }
+
+        }
+        private static bool BlowWillDoDamageToVictim(Agent attacker, Agent victim, Blow b, ref AttackCollisionData collisionData)
+        {
+            return !collisionData.AttackBlockedWithShield && b.SelfInflictedDamage < 0 && attacker.IsEnemyOf(victim) && !victim.IsMount;
+        }
+    }
+}
---
+++ HealthOnKill/HealthOnKillSettings.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Xml.Serialization;
+using ModLib.Definitions;
+using ModLib.Definitions.Attributes;
+
+namespace HealthOnKill
+{
+	public class HealthOnKillSettings : SettingsBase
+	{
+
+		public override string ModName
+		{
+			get
+			{
+				return "Health on Kill";
+			}
+		}
+
+
+		public override string ModuleFolderName
+		{
+			get
+			{
+				return "HealthOnKill";
+			}
+		}
+
+		[XmlElement]
+		public override string ID { get; set; } = "HealthOnKillSettings";
+
+
+		public static HealthOnKillSettings Instance
+		{
+			get
+			{
+				return (HealthOnKillSettings)SettingsDatabase.GetSettings<HealthOnKillSettings>();
+			}
+		}
+		[XmlElement]
+		[SettingProperty("Projectiles don't show on Player", "Projectiles will stick to player with this enabled")]
+		public bool MainStick { get; set; } = true;
+		[XmlElement]
+		[SettingProperty("Health on Kill Base Amount", 1f, 20f, "The base amount of health gained on kill.")]
+		public float HealthOnKillAmount { get; set; } = 8f;
+        [XmlElement]
+        [SettingProperty("Health on Kill Player Multiplier", 0f, 5f, "Multiplies base health on kill for player.")]
+        public float PlayerMultiplier { get; set; } = 1f;
+        [XmlElement]
+        [SettingProperty("Health on Kill Hero Multiplier",0f,5f, "Multiplies base health on kill for heroes .")]
+        public float HeroMultiplier{ get; set; } = 1f;
+        [XmlElement]
+        [SettingProperty("Health on Kill AI Multiplier", 0f, 5f, "Multiplies base health on kill for AI")]
+        public float AIMultiplier { get; set; } = 0f;
+		[XmlElement]
+		[SettingProperty("Health on Kill Mount Multiplier", 0f, 5f, "Multiplies base health on kill for mount (only applies to characters with over 0 multiplier)")]
+		public float MountMultiplier { get; set; } = 0f;
+		[XmlElement]
+        [SettingProperty("Health on Kill For Only Friendly troops", "With this enabled only friendly troops will heal on kills")]
+        public bool FriendlyOnly { get; set; } = true;
+		[XmlElement]
+		[SettingProperty("Damage Multiplier for Player", 1, 5, "Multiplies damage dealt by player.")]
+		public int DPlayerMultiplier { get; set; } = 1;
+		[XmlElement]
+		[SettingProperty("Damage Multiplier for Heroes", 1, 5, "Multiplies damage dealt by heroes .")]
+		public int DHeroMultiplier { get; set; } = 1;
+		[XmlElement]
+		[SettingProperty("Damage Multiplier for AI", 1, 5, "Multiplies damage dealt by AI")]
+		public int DAIMultiplier { get; set; } = 1;
+		[XmlElement]
+		[SettingProperty("Damage Multiplier For Only Friendly troops", "With this enabled only friendly troops will multiply damage dealt")]
+		public bool DFriendlyOnly { get; set; } = true;
+		[XmlElement]
+		[SettingProperty("Projectiles don't damage For Only Friendly troops", "With this enabled only friendly troops will not be damaged from projectiles")]
+		public bool FriendlyProjectiles { get; set; } = true;
+		[XmlElement]
+		[SettingProperty("Projectiles don't damage Player", "Projectiles will not damage or stick to player with this enabled")]
+		public bool MainProjectiles{ get; set; } = true;
+		[XmlElement]
+		[SettingProperty("Projectiles don't damage Heroes", "Projectiles will not damage or stick to heroes with this enabled")]
+		public bool HeroProjectiles { get; set; } = true;
+		[XmlElement]
+		[SettingProperty("Projectiles don't damage AI", "Projectiles will not damage or stick to AI with this enabled")]
+		public bool AIProjectiles { get; set; } = false;
+		[XmlElement]
+		[SettingProperty("Projectiles don't damage Mounts", "Projectiles will not damage or stick to Mounts whose riders aren't damaged by projectiles with this enabled")]
+		public bool MountProjectiles { get; set; } = true;
+
+		public const string InstanceID = "HealthOnKillSettings";
+	}
+}
