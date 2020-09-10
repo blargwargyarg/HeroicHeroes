a734482881c174549ea7249e3d21d4b37fcf1456
blargwargyarg <chujoe26@gmail.com>
2020-09-10 12:55:23 -0400

    Add files via upload

---
+++ HealthOnKill/HeroicHeroesPatch.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Windows.Forms;
+using HarmonyLib;
+using TaleWorlds.Engine;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace HeroicHeroes
+{
+    [HarmonyPatch(typeof(Mission))]
+    internal static class HeroicHeroesPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("GetStaggerThresholdMultiplier")]
+        public static bool Prefix1(Agent victimAgent, ref float __result)
+        {
+            try
+            {
+                if (victimAgent != null)
+                {
+                    if (victimAgent.IsMount)
+                    {
+                        if (HeroicHeroesSettings.Instance.MountProjectiles)
+                            if (victimAgent.RiderAgent != null)
+                                if ((victimAgent.RiderAgent.IsMainAgent && HeroicHeroesSettings.Instance.SPlayerMultiplier > 1 ||
+                                    (victimAgent.RiderAgent.IsHero && HeroicHeroesSettings.Instance.SHeroMultiplier > 1) ||
+                                    HeroicHeroesSettings.Instance.SAIMultiplier > 1) &&
+                                    checkFriendly(HeroicHeroesSettings.Instance.FriendlyProjectiles, victimAgent.RiderAgent)
+                                    ) return false;
+                    }
+                    else if (victimAgent.IsMainAgent)
+                    {
+                        __result = HeroicHeroesSettings.Instance.SPlayerMultiplier * MissionGameModels.Current.AgentApplyDamageModel.CalculateStaggerThresholdMultiplier(victimAgent);
+                        return false;
+                    }
+                    else if (victimAgent.IsHero)
+                    {
+                        if (true && checkFriendly(HeroicHeroesSettings.Instance.SFriendlyOnly, victimAgent))
+                        {
+                            __result = HeroicHeroesSettings.Instance.SHeroMultiplier * MissionGameModels.Current.AgentApplyDamageModel.CalculateStaggerThresholdMultiplier(victimAgent);
+                            return false;
+                        }
+                        else if (true && checkFriendly(HeroicHeroesSettings.Instance.SFriendlyOnly, victimAgent))
+                        {
+                            __result = HeroicHeroesSettings.Instance.SAIMultiplier * MissionGameModels.Current.AgentApplyDamageModel.CalculateStaggerThresholdMultiplier(victimAgent);
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch (Exception arg)
+            {
+                MessageBox.Show(string.Format("Error with GetStaggerThresholdMultiplier:\n\n{0}", arg));
+            }
+            return true;
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch("HandleMissileCollisionReaction")]
+        public static bool Prefix2(int missileIndex, ref Mission.MissileCollisionReaction collisionReaction,
+            MatrixFrame attachLocalFrame, Agent attackerAgent, Agent attachedAgent, bool attachedToShield,
+            sbyte attachedBoneIndex, MissionObject attachedMissionObject, Vec3 bounceBackVelocity, Vec3 bounceBackAngularVelocity, int forcedSpawnIndex)
+        {
+            try
+            {
+                if (attachedAgent != null && attackerAgent != null && 
+                    (!HeroicHeroesSettings.Instance.HeroProjectilesExemption || (attackerAgent.IsHuman && attackerAgent.IsHero)))
+                {
+                    if (attachedAgent.IsMount)
+                    {
+                        if (HeroicHeroesSettings.Instance.MountProjectiles)
+                            if (attachedAgent.RiderAgent != null)
+                                if ((attachedAgent.RiderAgent.IsMainAgent && HeroicHeroesSettings.Instance.MainProjectiles ||
+                                    (attachedAgent.RiderAgent.IsHero && HeroicHeroesSettings.Instance.HeroProjectiles) ||
+                                    HeroicHeroesSettings.Instance.AIProjectiles) &&
+                                    checkFriendly(HeroicHeroesSettings.Instance.FriendlyProjectiles, attachedAgent.RiderAgent)
+                                    ) collisionReaction = Mission.MissileCollisionReaction.BecomeInvisible;
+                    }
+                    else if (attachedAgent.IsMainAgent)
+                    {
+                        if (HeroicHeroesSettings.Instance.MainProjectiles || HeroicHeroesSettings.Instance.MainStick)
+                            collisionReaction = Mission.MissileCollisionReaction.BecomeInvisible;
+                    }
+                    else if (attachedAgent.IsHero)
+                    {
+                        if (HeroicHeroesSettings.Instance.HeroProjectiles)
+                            if (checkFriendly(HeroicHeroesSettings.Instance.FriendlyOnly, attachedAgent))
+                                collisionReaction = Mission.MissileCollisionReaction.BecomeInvisible;
+
+                    }
+                    else if (HeroicHeroesSettings.Instance.AIProjectiles && checkFriendly(HeroicHeroesSettings.Instance.FriendlyOnly, attachedAgent))
+                        collisionReaction = Mission.MissileCollisionReaction.BecomeInvisible;
+                }
+            }
+            catch (Exception arg)
+            {
+                MessageBox.Show(string.Format("Error with HandleMissileCollisionReaction:\n\n{0}", arg));
+            }
+            return true;
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch("RegisterBlow")]
+        public static bool Prefix3(Agent attacker, Agent victim, GameEntity realHitEntity, ref Blow b, ref AttackCollisionData collisionData)
+        {
+            try
+            {
+                if (attacker == null || victim == null) return true;
+                if (!(!collisionData.AttackBlockedWithShield && b.SelfInflictedDamage < 0 && attacker.IsEnemyOf(victim))) return true;
+                if (!HeroicHeroesSettings.Instance.DHeroExemption || (victim.IsHuman && victim.IsHero))
+                    if (attacker.IsMainAgent)
+                    {
+                        collisionData.InflictedDamage *= HeroicHeroesSettings.Instance.DPlayerMultiplier;
+                        b.InflictedDamage *= HeroicHeroesSettings.Instance.DPlayerMultiplier;
+                    }
+                    else if (attacker.IsHero)
+                    {
+                        if (checkFriendly(HeroicHeroesSettings.Instance.FriendlyOnly, attacker))
+                            b.InflictedDamage *= HeroicHeroesSettings.Instance.DHeroMultiplier;
+                    }
+                    else if (checkFriendly(HeroicHeroesSettings.Instance.FriendlyOnly, attacker))
+                        b.InflictedDamage *= HeroicHeroesSettings.Instance.DAIMultiplier;
+                if (b.IsMissile())
+                {
+                    if (!HeroicHeroesSettings.Instance.HeroProjectilesExemption || (attacker.IsHuman && attacker.IsHero))
+                        if (victim.IsMount)
+                        {
+                            if (HeroicHeroesSettings.Instance.MountProjectiles)
+                                if (victim.RiderAgent != null)
+                                    if ((victim.RiderAgent.IsMainAgent && HeroicHeroesSettings.Instance.MainProjectiles ||
+                                        (victim.RiderAgent.IsHero && HeroicHeroesSettings.Instance.HeroProjectiles) ||
+                                        HeroicHeroesSettings.Instance.AIProjectiles) &&
+                                        checkFriendly(HeroicHeroesSettings.Instance.FriendlyProjectiles, victim.RiderAgent)
+                                        ) return false;
+                        }
+                        else if (victim.IsMainAgent)
+                        {
+                            if (HeroicHeroesSettings.Instance.MainProjectiles)
+                                return false;
+                        }
+                        else if (victim.IsHero)
+                        {
+                            if (HeroicHeroesSettings.Instance.HeroProjectiles)
+                                if (checkFriendly(HeroicHeroesSettings.Instance.FriendlyProjectiles, victim))
+                                    return false;
+                        }
+                        else if (HeroicHeroesSettings.Instance.AIProjectiles && checkFriendly(HeroicHeroesSettings.Instance.FriendlyProjectiles, victim))
+                            return false;
+                }
+            }
+            catch (Exception arg)
+            {
+                MessageBox.Show(string.Format("Error with RegisterBlow Prefix:\n\n{0}", arg));
+            }
+            return true;
+        }
+        [HarmonyPostfix]
+        [HarmonyPatch("RegisterBlow")]
+        public static void Postfix(Agent attacker, Agent victim, GameEntity realHitEntity, Blow b, ref AttackCollisionData collisionData)
+        {
+            try
+            {
+                if (attacker == null || victim == null)
+                {
+                    return;
+                }
+                if (!BlowWillDoDamageToVictim(attacker, victim, b, ref collisionData) || victim.Health > 0f)
+                {
+                    return;
+                }
+                float toAdd = 0f;
+                if (attacker.IsMainAgent)
+                    toAdd = HeroicHeroesSettings.Instance.HealthOnKillAmount * HeroicHeroesSettings.Instance.PlayerMultiplier;
+                else if (attacker.IsHero)
+                {
+                    if (checkFriendly(HeroicHeroesSettings.Instance.FriendlyOnly, attacker))
+                        toAdd = HeroicHeroesSettings.Instance.HealthOnKillAmount * HeroicHeroesSettings.Instance.HeroMultiplier;
+                }
+                else if (checkFriendly(HeroicHeroesSettings.Instance.FriendlyOnly, attacker))
+                    toAdd = HeroicHeroesSettings.Instance.HealthOnKillAmount * HeroicHeroesSettings.Instance.AIMultiplier;
+                if (toAdd != 0f)
+                {
+                    attacker.Health = ((attacker.Health + toAdd < attacker.HealthLimit) ? (attacker.Health + toAdd) : attacker.HealthLimit);
+                    if (attacker.HasMount)
+                    {
+                        toAdd = HeroicHeroesSettings.Instance.HealthOnKillAmount * HeroicHeroesSettings.Instance.MountMultiplier;
+                        attacker.MountAgent.Health = ((attacker.MountAgent.Health + toAdd < attacker.MountAgent.HealthLimit) ? (attacker.MountAgent.Health + toAdd) : attacker.MountAgent.HealthLimit);
+                    }
+                }
+            }
+            catch (Exception arg)
+            {
+                MessageBox.Show(string.Format("Error with RegisterBlow Postfix:\n\n{0}", arg));
+            }
+
+        }
+        private static bool checkFriendly(bool b, Agent a)//checks if b (setting whether to check friendly or not)
+        {
+            return !b || (Agent.Main != null && a.IsFriendOf(Agent.Main));
+        }
+        private static bool BlowWillDoDamageToVictim(Agent attacker, Agent victim, Blow b, ref AttackCollisionData collisionData)
+        {
+            return !collisionData.AttackBlockedWithShield && b.SelfInflictedDamage < 0 && attacker.IsEnemyOf(victim) && !victim.IsMount;
+        }
+    }
+}
---
+++ HealthOnKill/HeroicHeroesSettings.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Xml.Serialization;
+using ModLib.Definitions;
+using ModLib.Definitions.Attributes;
+
+namespace HeroicHeroes
+{
+	public class HeroicHeroesSettings : SettingsBase
+	{
+
+		public override string ModName
+		{
+			get
+			{
+				return "Heroic Heroes";
+			}
+		}
+
+
+		public override string ModuleFolderName
+		{
+			get
+			{
+				return "HeroicHeroes";
+			}
+		}
+
+		[XmlElement]
+		public override string ID { get; set; } = "HeroicHeroesSettings";
+
+
+		public static HeroicHeroesSettings Instance
+		{
+			get
+			{
+				return (HeroicHeroesSettings)SettingsDatabase.GetSettings<HeroicHeroesSettings>();
+			}
+		}
+		[XmlElement]
+		[SettingProperty("Projectiles don't show on Player", "Projectiles will stick to player with this enabled")]
+		public bool MainStick { get; set; } = true;
+		[XmlElement]
+		[SettingProperty("Health on Kill Base Amount", 1f, 20f, "The base amount of health gained on kill.")]
+		public float HealthOnKillAmount { get; set; } = 8f;
+        [XmlElement]
+        [SettingProperty("Health on Kill Multiplier for Player", 0f, 5f, "Multiplies base health on kill for player.")]
+        public float PlayerMultiplier { get; set; } = 1f;
+        [XmlElement]
+        [SettingProperty("Health on Kill Multiplier for Heores",0f,5f, "Multiplies base health on kill for heroes .")]
+        public float HeroMultiplier{ get; set; } = 1f;
+        [XmlElement]
+        [SettingProperty("Health on Kill Multiplier for AI", 0f, 5f, "Multiplies base health on kill for AI")]
+        public float AIMultiplier { get; set; } = 0f;
+		[XmlElement]
+		[SettingProperty("Health on Kill Multiplier for Mounts", 0f, 5f, "Multiplies base health on kill for mount (only applies to characters with over 0 multiplier)")]
+		public float MountMultiplier { get; set; } = 0f;
+		[XmlElement]
+        [SettingProperty("Health on Kill For Only Friendly troops", "With this enabled only friendly troops will heal on kills")]
+        public bool FriendlyOnly { get; set; } = false;
+		[XmlElement]
+		[SettingProperty("Damage Multiplier for Player", 1, 10, "Multiplies damage dealt by player.")]
+		public int DPlayerMultiplier { get; set; } = 1;
+		[XmlElement]
+		[SettingProperty("Damage Multiplier for Heroes", 1, 10, "Multiplies damage dealt by heroes .")]
+		public int DHeroMultiplier { get; set; } = 1;
+		[XmlElement]
+		[SettingProperty("Damage Multiplier for AI", 1, 10, "Multiplies damage dealt by AI")]
+		public int DAIMultiplier { get; set; } = 1;
+		[XmlElement]
+		[SettingProperty("Damage Multiplier Hero Exemption", "Heroes are exempt from additional damage multipliers")]
+		public bool DHeroExemption { get; set; } = true;
+
+		[XmlElement]
+		[SettingProperty("Damage Multiplier Only For Friendly troops", "With this enabled only friendly troops will multiply damage dealt")]
+		public bool DFriendlyOnly { get; set; } = false;
+		[SettingProperty("Stagger Threshold Multiplier for Player", 1, 20, "Multiplies stagger thresholddealt by player.")]
+		public int SPlayerMultiplier { get; set; } = 1;
+		[XmlElement]
+		[SettingProperty("Stagger Threshold Multiplier for Heroes", 1, 20, "Multiplies stagger threshold dealt by heroes .")]
+		public int SHeroMultiplier { get; set; } = 1;
+		[XmlElement]
+		[SettingProperty("Stagger Threshold Multiplier for AI", 1, 20, "Multiplies stagger threshold for AI")]
+		public int SAIMultiplier { get; set; } = 1;
+		[XmlElement]
+		[SettingProperty("Stagger Threshold Multiplier for Mounts", 1, 20, "Multiplies stagger threshold for Mounts whose riders have stagger threshold multipliers")]
+		public int SMountMultiplier { get; set; } = 1;
+		[XmlElement]
+		[SettingProperty("Stagger Threshold Multiplier Only For Friendly troops", "With this enabled only friendly troops will multiply damage dealt")]
+		public bool SFriendlyOnly { get; set; } = false;
+		[XmlElement]
+		[SettingProperty("Projectiles don't damage For Only Friendly troops", "With this enabled only friendly troops will not be damaged from projectiles")]
+		public bool FriendlyProjectiles { get; set; } = false;
+		[XmlElement]
+		[SettingProperty("Projectiles don't damage setting for Player", "Projectiles will not damage or stick to player with this enabled")]
+		public bool MainProjectiles{ get; set; } = true;
+		[XmlElement]
+		[SettingProperty("Projectiles don't damage setting for Heroes", "Projectiles will not damage or stick to heroes with this enabled")]
+		public bool HeroProjectiles { get; set; } = true;
+		[XmlElement]
+		[SettingProperty("Projectiles don't damage setting for AI", "Projectiles will not damage or stick to AI with this enabled")]
+		public bool AIProjectiles { get; set; } = false;
+		[XmlElement]
+		[SettingProperty("Projectiles don't damage Mounts", "Projectiles will not damage or stick to Mounts whose riders aren't damaged by projectiles with this enabled")]
+		public bool MountProjectiles { get; set; } = false;
+		[XmlElement]
+		[SettingProperty("Projectiles don't damage Hero exemption", "Heroes-fired projectiles will damage the target")]
+		public bool HeroProjectilesExemption { get; set; } = true;
+
+
+		public const string InstanceID = "HeroicHeroesSettings";
+	}
+}
